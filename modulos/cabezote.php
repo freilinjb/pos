15e3b49f2b1da068ad7851edc9a512bd541f2634
freilinjb <freilinjb@gmail.com>
2020-08-28 07:20:27 -0400

    agrergando boton perfil

--- modulos/cabezote.php
+++ modulos/cabezote.php
@@ -1,17 +1,40 @@
 <header class="main-header">
-    <!-- LOGOTIPO -->
+    <!-- Boton de navegacion -->
     <a href="" class="logo">
         <!-- LOGO mini -->
         <span class="logo-mini">
 
-            <img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive"
-                 style="padding: 10px;" alt="">
+            <img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive" style="padding: 10px;" alt="">
         </span>
         <!-- LOGO normal -->
-        <span class="logo-mini">
-            
-            <img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive"
-                 style="padding: 10px 0px;" alt="">
+        <span class="logo-lg">
+
+            <img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding: 10px 0px;" alt="">
         </span>
-    </a> 
+    </a>
+
+    <!-- =========================== -->
+    <!-- BARRA DE NAVEGACION -->
+    <!-- =========================== -->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <!-- Perfil de usuario -->
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="vistas/img/usuarios/default/anonymous.png " class="user-image" alt="User Image">
+                        <span class="hidden-xs">Usuario Administrador</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- Dropdown-toggle -->
+        
+    </nav>
+
 </header>
