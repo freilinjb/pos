ca24c8d0c994ed5306e2aaa429abc33971d15334
freilinjb <freilinjb@gmail.com>
2020-08-28 06:59:14 -0400

    modularizando el cabezote

---
+++ modulos/cabezote.php
@@ -0,0 +1,17 @@
+<header class="main-header">
+    <!-- LOGOTIPO -->
+    <a href="" class="logo">
+        <!-- LOGO mini -->
+        <span class="logo-mini">
+
+            <img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive"
+                 style="padding: 10px;" alt="">
+        </span>
+        <!-- LOGO normal -->
+        <span class="logo-mini">
+            
+            <img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive"
+                 style="padding: 10px 0px;" alt="">
+        </span>
+    </a> 
+</header>
