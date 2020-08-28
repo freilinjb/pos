a19a3e626dd50dadfbba7744528cc6d0fa6ccb03
freilinjb <freilinjb@gmail.com>
2020-08-28 13:31:51 -0400

    creando modelos y controladores

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
 
 require_once "controladores/plantilla.controlador.php";
+require_once "controladores/usuarios.controlador.php";
+require_once "controladores/categorias.controlador.php";
+require_once "controladores/productos.controlador.php";
+require_once "controladores/clientes.controlador.php";
+require_once "controladores/ventas.controlador.php";
+
+require_once "controladores/usuarios.modelo.php";
+require_once "controladores/categorias.modelo.php";
+require_once "controladores/productos.modelo.php";
+require_once "controladores/clientes.modelo.php";
+require_once "controladores/ventas.modelo.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
