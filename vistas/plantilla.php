22fb61cc6de4ec1e5cbc12931d71b441ac098158
freilinjb <freilinjb@gmail.com>
2020-08-28 20:19:41 -0400

    agregando icono

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -4,8 +4,13 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Inventory System</title>
+
+
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  
+  <link rel="icon" href="vistas/img/plantilla/icono-negro.png">
+
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
 
