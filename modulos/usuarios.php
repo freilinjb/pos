18e3b38555daec50e744fba79d6146bc41b50c08
freilinjb <freilinjb@gmail.com>
2020-08-30 08:31:41 -0400

    maquetando el modal de usuario

--- modulos/usuarios.php
+++ modulos/usuarios.php
@@ -8,7 +8,7 @@
             <small>Panel de control</small>
 
         </h1>
-        
+
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
             <li class="active">Administrar usuarios</li>
@@ -51,7 +51,7 @@
                                 <button class="btn btn-success btn-xs">Activado</button>
                             </td>
                             <td>2017-12-11 12:05:32</td>
-                            
+
                             <td>
                                 <div class="btn-group">
                                     <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
@@ -69,7 +69,7 @@
                                 <button class="btn btn-success btn-xs">Activado</button>
                             </td>
                             <td>2017-12-11 12:05:32</td>
-                            
+
                             <td>
                                 <div class="btn-group">
                                     <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
@@ -91,23 +91,79 @@
 
 <!-- MODAL AGRERGAR USUARIO -->
 
-<!-- Modal -->
 <div id="modalAgrergarUsuario" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+    <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
+        <div class="modal-content">
+            <!-- enctype es para enviar archivos -->
+            <form role="form" method="post" enctype="multipart/form-data">
+                <!-- CABEZA -->
+                <div class="modal-header" style="background: #3c8dbc; color:white">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Agregar usuario</h4>
+                </div>
+                <div class="modal-body">
+
+                    <div class="box-body">
+                        <!-- ENTRADA PARA EL USUARIO -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+
+                                <input type="text" class="form-control input-lg " name="nuevoNombre" placeholder="Ingresar nombre" require>
+                            </div>
+                        </div>
+
+                        <!-- ENTRADA PARA EL USUARIO -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+
+                                <input type="text" class="form-control input-lg " name="nuevoUsuario" placeholder="Ingresar usuario" require>
+                            </div>
+                        </div>
+                        <!-- ENTRADA PARA EL CONTRASEÑA -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+
+                                <input type="text" class="form-control input-lg " name="nuevoPassword" placeholder="Ingresar contraseña" require>
+                            </div>
+                        </div>
+
+                        <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+
+                                <select class="form-control input-lg" name="nuevoPerfil">
+                                    <option value="">Seleccionar perfil</option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Especial">Especial</option>
+                                    <option value="Vendedor">Vendedor</option>
+                                </select>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
+                    <div class="form-group">
+                        <div class="panel">SUBIR FOTO</div>
+                        <input type="file" id="nuevaFoto" name="nuevaFoto">
+
+                        <p class="help-block">Peso máximo de la foto 200 mb</p>
+                        <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumdnail" width="100px" alt="Foto">
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+                        <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
     </div>
 
-  </div>
 </div>
