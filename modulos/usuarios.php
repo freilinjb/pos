66a1fd618d80468982a66193e3725fca2432c57c
freilinjb <freilinjb@gmail.com>
2020-09-05 22:26:47 -0400

    Listando registros de usuarios

--- modulos/usuarios.php
+++ modulos/usuarios.php
@@ -42,49 +42,43 @@
 
                     <tbody>
                     <?php
-                      $item = null;
-                      $valor = null;
+                        $item = null;
+                        $valor = null;
 
-                      $usuarios = ControladorUsuarios::ctrMostrarUsuarios($item, $valor);
+                        $usuarios = ControladorUsuarios::ctrMostrarUsuarios($item, $valor);
 
-                      var_dump($usuarios);
+                        foreach($usuarios as $key => $value) {
+                            echo '<tr>
+                            <td>'.$value['id'].'</td>
+                            <td>'.$value['nombre'].'</td>
+                            <td>'.$value['usuario'].'</td>';
+
+                            if($value['foto'] != "") {
+
+                              echo '<td><img src="'.$value['foto'].'" class="img-thumbnail" width="40px" alt="Foto"></td>';
+
+                            } else {
+
+                              echo '<td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px" alt="Foto"></td>';
+
+                            }
+                            
+                            echo '<td>'.$value['perfil'].'</td>
+                            <td>
+                                <button class="btn btn-success btn-xs">Activado</button>
+                            </td>
+                            <td>'.$value['ultimo_login'].'</td>
+
+                            <td>
+                                <div class="btn-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>';
+                        }
                     ?>
-                        <tr>
-                            <td>1</td>
-                            <td>Usuario Administrador</td>
-                            <td>Administrador</td>
-                            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px" alt="Foto"></td>
-                            <td>Administrador</td>
-                            <td>
-                                <button class="btn btn-success btn-xs">Activado</button>
-                            </td>
-                            <td>2017-12-11 12:05:32</td>
-
-                            <td>
-                                <div class="btn-group">
-                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Usuario Administrador</td>
-                            <td>Administrador</td>
-                            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail previsualizar" width="40px" alt="Foto"></td>
-                            <td>Administrador</td>
-                            <td>
-                                <button class="btn btn-success btn-xs">Activado</button>
-                            </td>
-                            <td>2017-12-11 12:05:32</td>
-
-                            <td>
-                                <div class="btn-group">
-                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-                                </div>
-                            </td>
-                        </tr>
+                        
                     </tbody>
                 </table>
             </div>
