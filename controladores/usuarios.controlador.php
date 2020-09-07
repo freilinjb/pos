7c02b6cb3d4ac17c635ba99270b224bee168c265
freilinjb <freilinjb@gmail.com>
2020-09-07 07:40:57 -0400

    actualizando ultimo_login en la DB

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -42,6 +42,30 @@ class ControladorUsuarios
 						$_SESSION["usuario"] = $respuesta['usuario'];
 						$_SESSION["foto"] = $respuesta['foto'];
 						$_SESSION["perfil"] = $respuesta['perfil'];
+
+						/**
+						 * todo REGISTRAR FECHA PARA SABER EL ÚLTIMO LOGIN
+						 */
+						date_default_timezone_set('America/Santo_Domingo');
+						$fecha = date('Y-m-d');
+						$hora = date('H:i:s');
+
+						$fechaActual = $fecha.' '.$hora;
+
+						$item1 = "ultimo_login";
+						$valor1 = $fechaActual;
+						
+						$item2 = "id";
+						$valor2 = $respuesta["id"];
+
+						$ultimoLogin = ModeloUsuarios::mdlActualizarUsuario($tabla, $item1, $valor1, $item2, $valor2);
+
+						if($ultimoLogin == "ok") {
+
+							echo '<script>
+									window.location = "inicio";	
+								</script>';
+						}
 	
 						echo '<script>
 	
