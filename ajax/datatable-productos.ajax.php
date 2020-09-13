671b72430380b811d43d712d7f014c9e90933838
freilinjb <freilinjb@gmail.com>
2020-09-13 17:50:38 -0400

    agrergando botones de cantidad y idiomas

--- ajax/datatable-productos.ajax.php
+++ ajax/datatable-productos.ajax.php
@@ -23,27 +23,51 @@ class TablaProductos {
 
         $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
 
-        $botones =  "<div class='btn-group'><button class='btn btn-warning'><i class='fa fa-pencil'></i></button><button class='btn btn-danger'><i class='fa fa-times'></i></button></div>";        
-
-        
-        // print_r($productos);
-
         $datosJson = '{
             "data": [';
 
         $length = count($productos);
 
+        #btn Eliminar se le agrega codigo y foto a la clase para 
+        #identificarlo y poder cambiarlos
+
         for($i = 0; $i < $length; $i++) {
 
+            #AGREGA LA IMAGEN
             $imagen = "<img src='".$productos[$i]["imagen"]."' width='40px'>";
+            #AGRERGA LOS BOTONES CON SUS ID DEL PRODUCTO
+            $botones =  "<div class='btn-group'><button class='btn btn-warning btnEditarProducto' idProducto='".$productos[$i]["id"]."' data-toggle='modal' data-target='#modalEditarProducto'><i class='fa fa-pencil'></i></button><button class='btn btn-danger btnEliminarProducto' idProducto='".$productos[$i]['id']."' codigo='".$productos[$i]['codigo']."' imagen='".$productos[$i]['imagen']."'><i class='fa fa-times'></i></button></div>";        
 
+            #Filtra la categoria por el id
+            #CONSULTA LA CATEGORIA A LA QUE PERTENECE EL PRODUCTO
+            $item = "id";
+            $valor = $productos[$i]["id_categoria"];
+
+            #Consulta el id de la categoria para mostrarla
+            $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+            #STOCK
+            $clase = null;
+            if($productos[$i]["stock"] <= 10) {
+                $clase = "danger";
+
+            } else if ($productos[$i]["stock"] > 11 && $productos[$i]["stock"] <= 15){ 
+                $clase = "warning";
+
+            } else {
+                $clase = "success";
+            }
+
+            $stock = "<button class='btn btn-".$clase."'>".$productos[$i]["stock"]."</button>";
+            
+            #CONSTRULLE EL JSON PARA EL DATATABLE
             $datosJson .= '[
                 "'.($i+1).'",
                 "'.$imagen.'",
                 "'.$productos[$i]["codigo"].'",
                 "'.$productos[$i]["descripcion"].'",
-                "Taladros",
-                "'.$productos[$i]["stock"].'",
+                "'.$categoria['categoria'].'",
+                "'.$stock.'",
                 "'.$productos[$i]["precio_compra"].'",
                 "'.$productos[$i]["precio_venta"].'",
                 "'.$productos[$i]["fecha"].'",
