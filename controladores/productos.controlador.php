1e4cd8e20aa4746b649818307f9a35049ab51ff0
freilinjb <freilinjb@gmail.com>
2020-09-19 14:04:09 -0400

    Subiendo fotos del producto

--- controladores/productos.controlador.php
+++ controladores/productos.controlador.php
@@ -26,8 +26,67 @@ class ControladorProductos {
             preg_match('/^[0-9]+$/', $_POST["nuevoStock"]) &&
             preg_match('/^[0-9,.]+$/', $_POST["nuevoPrecioCompra"]) &&
             preg_match('/^[0-9,.]+$/', $_POST["nuevoPrecioVenta"])) {
-                    
-                $ruta = "vistas/img/productos/default/anonymous.php";
+
+                //VALIDAR IMAGEN
+                $ruta = "vistas/img/productos/default/anonymous.png";
+				//VALIDAR IMAGEN
+				if(isset($_FILES['nuevaImagen']['tmp_name'])) {
+					//Crear nuevo array
+					list($ancho, $alto) = getimagesize($_FILES['nuevaImagen']['tmp_name']);
+					
+					//Redimencionar
+					$nuevoAncho = 500;
+					$nuevoAlto = 500;
+
+					$directorio = 'vistas/img/productos/'.$_POST['nuevoCodigo'];
+					
+					//0755 permiso de lectura y estricura
+					mkdir($directorio, 0755);
+
+					/**
+					 * De acuerdo al tipo de imagen aplicamos las funciones por defecto
+					 * de PHP
+					 */
+
+					 if($_FILES['nuevaImagen']['type'] == 'image/jpeg') {
+						 /**
+						  * Guardar la imagen em el directorio
+						  */
+
+						  $aleatorio = mt_rand(100,999);
+
+						  $ruta = "vistas/img/usuarios/".$_POST['nuevoCodigo']."/".$aleatorio.".jpg";
+  
+						  $origen = imagecreatefromjpeg($_FILES["nuevaImagen"]["tmp_name"]);						
+  
+						  $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+  
+						  imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+  
+						  imagejpeg($destino, $ruta);
+					 }
+
+					 if($_FILES['nuevaImagen']['type'] == 'image/png') {
+						/**
+						 * Guardar la imagen em el directorio
+						 */
+
+						 $aleatorio = mt_rand(100,999);
+
+						 $ruta = "vistas/img/productos/".$_POST['nuevoCodigo']."/".$aleatorio.".png";
+ 
+						 $origen = imagecreatefrompng($_FILES["nuevaImagen"]["tmp_name"]);						
+ 
+						 $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+ 
+						 imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+ 
+						 imagepng($destino, $ruta);
+					}
+
+				}
+
+
 
                 $tabla = "productos";
                 $datos = array("id_categoria" => $_POST['nuevaCategoria'],
