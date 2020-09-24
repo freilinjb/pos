39ca51cbee222953151fcdadcef1d3eaace4102d
freilinjb@gmail.com <freilinjb@gmail.com>
2020-09-24 12:07:51 -0400

    consultando y mostrando producto al hacer click

--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -2,55 +2,70 @@
  * @todo CARGAR LA TABLA DINÁMICA DE VENTAS
  */
 
- $.ajax({
-    url: 'ajax/datatable-ventas.ajax.php',
+// $.ajax({
+//   url: "ajax/datatable-ventas.ajax.php",
+//   success: function (respuesta) {
+//     console.log("respuesta: ", respuesta);
+//   },
+// });
+
+$(".tablaVentas").DataTable({
+  ajax: "ajax/datatable-ventas.ajax.php",
+  deferRender: true,
+  retrieve: true,
+  processing: true,
+  language: {
+    sProcessing: "Procesando...",
+    sLengthMenu: "Mostrar _MENU_ registros",
+    sZeroRecords: "No se encontraron resultados",
+    sEmptyTable: "Ningún dato disponible en esta tabla",
+    sInfo:
+      "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+    sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+    sInfoPostFix: "",
+    sSearch: "Buscar:",
+    sUrl: "",
+    sInfoThousands: ",",
+    sLoadingRecords: "Cargando...",
+    oPaginate: {
+      sFirst: "Primero",
+      sLast: "Último",
+      sNext: "Siguiente",
+      sPrevious: "Anterior",
+    },
+    oAria: {
+      sSortAscending: ": Activar para ordenar la columna de manera ascendente",
+      sSortDescending:
+        ": Activar para ordenar la columna de manera descendente",
+    },
+    buttons: {
+      copy: "Copiar",
+      colvis: "Visibilidad",
+    },
+  },
+});
+
+$(".tablaVentas tbody").on("click", "button.agregarProducto", function () {
+  const idProducto = $(this).attr("idProducto");
+
+  $(this).removeClass("btn-primary agregarProducto");
+
+  $(this).addClass("btn-default");
+
+  const datos = new FormData();
+  datos.append("idProducto", idProducto);
+
+  $.ajax({
+    url: "ajax/productos.ajax.php",
+    method: "POST",
+    data: datos,
+    cache: false,
+    contentType: false,
+    processData: false,
+    dataType:"json",
     success: function(respuesta) {
-        console.log('respuesta: ', respuesta);
+        console.log("respuesta: ", respuesta);
     }
- });
-
- 
- $('.tablaVentas').DataTable( {
-    "ajax": "ajax/datatable-ventas.ajax.php",
-    "deferRender": true,
-    "retrieve": true,
-    "processing": true,
-    "language": {
-        "sProcessing":     "Procesando...",
-        "sLengthMenu":     "Mostrar _MENU_ registros",
-        "sZeroRecords":    "No se encontraron resultados",
-        "sEmptyTable":     "Ningún dato disponible en esta tabla",
-        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-        "sInfoPostFix":    "",
-        "sSearch":         "Buscar:",
-        "sUrl":            "",
-        "sInfoThousands":  ",",
-        "sLoadingRecords": "Cargando...",
-        "oPaginate": {
-            "sFirst":    "Primero",
-            "sLast":     "Último",
-            "sNext":     "Siguiente",
-            "sPrevious": "Anterior"
-        },
-        "oAria": {
-            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-        },
-        "buttons": {
-            "copy": "Copiar",
-            "colvis": "Visibilidad"
-        }
-    }
-} );
-
-
-$('.tablaVentas tbody').on('click','button.agregarProducto', function() {
-    const idProducto = $(this).attr('idProducto');
-
-    console.log('idProducto:',idProducto);
-    
-    $(this).removeClass('btn-primary agregarProducto');
-    $(this).addClass('btn-default')
+  });
 });
